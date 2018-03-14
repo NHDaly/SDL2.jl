834a533d0e1be08c09ec13e37a4c9dfcf71c45be
Nathan Daly <nhdaly@gmail.com>
2018-03-13 21:59:12 -0500

    added libsdl2-2.0-0 alias for linux apt-get
    Set AptGet to use it

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using Compat
 
 @BinDeps.setup
 
-libSDL2 = library_dependency("libSDL2", aliases = ["sdl2", "libsdl2-2.0", "libSDL","SDL2"])
+libSDL2 = library_dependency("libSDL2", aliases = ["sdl2", "libsdl2-2.0", "libsdl2-2.0-0", "libSDL","SDL2"])
 libSDL2_ttf = library_dependency("libSDL2_ttf", aliases = ["SDL_ttf","SDL2_ttf"])
 
 if is_apple()
@@ -18,7 +18,7 @@ if is_windows()
     provides(Binaries, URI("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14-win32-x64.zip"), libSDL2_ttf, unpacked_dir=".")
 end
 
-provides(AptGet, "libsdl2-2.0", libSDL2)
+provides(AptGet, "libsdl2-2.0-0", libSDL2)
 provides(Yum, "SDL2", libSDL2)
 provides(Pacman, "sdl2", libSDL2)
 
